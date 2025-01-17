7cd2df0e18550c010f12f2fb1e897c1b8db2d7c6
Denys Leon <leonden0046@gmail.com>
2025-01-17 15:20:45 +0200

    _

--- Assets/Scripts/Player/Mage.cs
+++ Assets/Scripts/Player/Mage.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Net.NetworkInformation;
 using UnityEditor.U2D.Sprites;
-=======
->>>>>>> bcba07212653e8710bd4ff3ca968c1ebe42ec1c4
 using UnityEngine;
 
 public class Mage : PlayerBase
 {
-<<<<<<< HEAD
     [SerializeField] private Transform shootingPoint;
     [SerializeField] private GameObject projectilePrefab; 
 
@@ -24,10 +20,5 @@ public class Mage : PlayerBase
         mageProjectile.SetIsMovingRight(IsFacingRight);
 
         projectileSpriteRenderer.flipX = shouldFlipSpriteOnX;
-=======
-    public override void Attack()
-    {
-        Debug.Log("Mage Attack");
->>>>>>> bcba07212653e8710bd4ff3ca968c1ebe42ec1c4
     }
 }
