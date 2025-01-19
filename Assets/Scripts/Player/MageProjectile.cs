752d0e917346cc02c50c9defa26e6635dfa898fd
Denys Leon <leonden0046@gmail.com>
2025-01-19 15:35:22 +0200

    Mage dealing damage

--- Assets/Scripts/Player/MageProjectile.cs
+++ Assets/Scripts/Player/MageProjectile.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public class MageProjectile : MonoBehaviour
 {
+    [SerializeField] private int projectileDamage = 1;
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float destroyTimeAfterInitialization = 10f; // if it goest beyond the map, destroy it after destroyTimeAfterInitialization seconds
     private bool isMovingRight = true;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, destroyTimeAfterInitialization);
+    }
 
     private void Update()
     {
@@ -21,6 +28,14 @@ public class MageProjectile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Health targetHealth = collision.transform.GetComponent<Health>();
+
+        if(targetHealth != null)
+        {
+            targetHealth.TakeDamage(projectileDamage);
+        }
+
         Destroy(this.gameObject);
     }
+
 }
